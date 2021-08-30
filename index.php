bd9adab0bfb2089ec892efed7f5e2a26b9894ad1
Kafanakone <kafana.kone@scream.ci>
2021-08-30 11:30:54 +0000

    version 1

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulaire</title>
+</head>
+<body>
+    <div class="conteneur">
+        <div class="mini-conteneur-1">
+            <h2>Login</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Nulla non aperiam cum quas quod reprehenderit.</p>
+                <ul class="reseauS">
+                    <li> <a href="#" class="facebook">Facebook</a></li>
+                    <li><a href="#" class="linkedin">Linkedin</a></li>
+                </ul>
+        </div>
+        <div class="clear"></div>
+        <div class="mini-conteneur-2">
+            <h2>Login Form</h2>
+            <form action="index.php">
+                <label for="mail"></label>
+                <input type="text" placeholder="Votre Mail" id="mail" required>
+                <label for="pwd"></label>
+                <input type="password" placeholder="Votre Mail" id="pwd" required>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
